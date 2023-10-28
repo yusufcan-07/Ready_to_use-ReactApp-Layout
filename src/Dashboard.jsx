f289cdefb0f565473e95dbb6fa925c72dfaaee19
Yusufcan Ulusoy <yusufcan.ulusoy@agu.edu.tr>
2023-10-28 20:10:38 +0300

    Some minur bugs solved

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -4,7 +4,6 @@ import Card from "./Card";
 const Dashboard = () => {
   return (
     <>
-      My Portfolio
       <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10">
         <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1">
           <Card>My Portfolio</Card>
