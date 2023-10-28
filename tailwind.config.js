308bd6ca6ebbe1cc945ce39ce685bd07c1b854a5
Yusufcan Ulusoy <yusufcan.ulusoy@agu.edu.tr>
2023-10-28 20:19:38 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
-const plugin = require("tailwindcss/plugin");
-
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        white: "white",
-        none: "none",
-      },
-      fontFamily: {
-        quicksand: ["Quicksand", "sans-serif"],
-      },
-      borderWidth: {
-        1: "1px",
-      },
       gridTemplateRows: {
         7: "repeat(7, minmax(0, 1fr))",
         8: "repeat(8, minmax(0, 1fr))",
